315293b086b2ff6c45c2b9dd3422f6480e4bf5a4
Aggelos Orfanakos <me@agorf.gr>
2014-02-23 18:35:05 +0200

    Do not raise exception when save fails
    This is to make it possible to skip already indexed photographs.

--- lib/thyme/db.rb
+++ lib/thyme/db.rb
@@ -1,5 +1,4 @@
 require 'data_mapper'
 
 DataMapper::Logger.new(STDOUT, :debug)
-DataMapper::Model.raise_on_save_failure = true
 DataMapper.setup(:default, "sqlite://#{File.expand_path('index.db')}")
