658302513b5e30ddb2e6072860e7125c445df9ab
Aggelos Orfanakos <me@agorf.gr>
2015-02-04 18:45:20 +0100

    Remove Hash#translate_keys
    Not used since 8a2a477

--- lib/thyme/core_ext.rb
+++ lib/thyme/core_ext.rb
@@ -18,15 +18,4 @@ class Hash
   def camelize_keys
     Hash[map {|k, v| [k.to_s.camelize, v] }]
   end
-
-  def translate_keys(map)
-    hash = dup
-
-    map.each do |from, to|
-      hash[to] = hash[from]
-      hash.delete(from)
-    end
-
-    hash
-  end
 end
