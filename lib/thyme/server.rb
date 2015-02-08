5e0bcc7eee6cec04b060a87d14c74fc4ec6d5a08
Aggelos Orfanakos <me@agorf.gr>
2015-02-08 15:03:18 +0100

    Cleanup Server

--- lib/thyme/server.rb
+++ lib/thyme/server.rb
@@ -9,51 +9,54 @@ module Thyme
   class Server < Sinatra::Base
     set :root, File.expand_path('../../..', __FILE__)
 
-    get '/' do
+    helpers do
+      def require_json
+        pass unless request.accept?('application/json')
+      end
+    end
+
+    get '/', provides: :html do
       send_file File.expand_path('index.html', settings.public_folder)
     end
 
-    get '/sets' do
-      pass unless request.accept?('application/json')
-      content_type :json
+    get '/sets', provides: :json do
       Set.newest_first.to_json
     end
 
-    get '/set' do
-      pass unless request.accept?('application/json')
+    # GET /set?id=:id
+    get '/set', provides: :json do
+      require_json
 
       if set = Set[params[:id]]
-        content_type :json
         set.to_json
       else
-        halt 404, 'Not Found'
+        halt 404, '{}'
       end
     end
 
-    get '/photos' do
-      pass unless request.accept?('application/json')
-      content_type :json
+    # GET /photos?set_id=:set_id
+    get '/photos', provides: :json do
+      require_json
       Photo.where(set_id: params[:set_id]).oldest_first.to_json
     end
 
-    get '/photo' do
-      pass unless request.accept?('application/json')
+    # GET /photo?id=:id
+    get '/photo', provides: :json do
+      require_json
 
       if photo = Photo[params[:id]]
-        content_type :json
         photo.to_json
       else
-        halt 404, 'Not Found'
+        halt 404, '{}'
       end
     end
 
-    get '/config' do
-      pass unless request.accept?('application/json')
-      content_type :json
+    get '/config', provides: :json do
+      require_json
       Hash[
         *%w{mapbox_map_id mapbox_token}.map {|key|
-          [key, ENV[key.upcase]]
-        }.flatten
+          [key, ENV[key.upcase]] if ENV.has_key?(key.upcase)
+        }.compact.flatten
       ].to_json
     end
   end
