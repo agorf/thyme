beddaeb265944b30ed34b3fa5c24b591c353489f
Aggelos Orfanakos <me@agorf.gr>
2015-02-09 19:04:25 +0100

    Store set thumb_url and photos count in database
    This leads to significant performance gains when there are a lot of
    photos.

--- lib/thyme/db.rb
+++ lib/thyme/db.rb
@@ -11,6 +11,8 @@ DB.loggers << Logger.new($stderr) if ENV['THYME_ENV'] == 'development'
 DB.create_table? :sets do
   primary_key :id
   String :name, size: 4096, null: false, unique: true
+  String :thumb_url, null: false, unique: true
+  Integer :photos_count, null: false
   Time :taken_at
 end
 
--- lib/thyme/set.rb
+++ lib/thyme/set.rb
@@ -12,24 +12,5 @@ module Thyme
         reverse_order(:taken_at)
       end
     end
-
-    def to_json(options = {})
-      super(
-        options.merge(include: [
-          :photos_count,
-          :thumb_url,
-        ])
-      )
-    end
-
-    private
-
-    def photos_count
-      photos.count
-    end
-
-    def thumb_url
-      photos_dataset.oldest_first.first.small_thumb_url
-    end
   end
 end
