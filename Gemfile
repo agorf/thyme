1c8ef781ac93d0229f582b2f2369eac6a0022ca5
Aggelos Orfanakos <me@agorf.gr>
2015-02-15 15:18:26 +0100

    Remove stuff that is no longer that useful

--- Gemfile
+++ Gemfile
@@ -3,9 +3,3 @@ source 'https://rubygems.org'
 gem 'sequel'
 gem 'sinatra'
 gem 'sqlite3'
-
-group :development do
-  gem 'pry'
-  gem 'rerun'
-  gem 'thin'
-end
--- lib/thyme/db.rb
+++ lib/thyme/db.rb
@@ -5,5 +5,3 @@ DB = Sequel.connect(
   adapter: 'sqlite',
   database: File.expand_path('../../../thyme.db', __FILE__)
 )
-
-DB.loggers << Logger.new($stderr) if ENV['THYME_ENV'] == 'development'
